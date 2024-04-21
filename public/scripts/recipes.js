dc24f8b26c0387034cb5794f590cebe1530b0dc4
Dima-McArrow <dsm.box@gmail.com>
2024-04-22 00:59:06 +0200

    changes v21 html updated, js for f-e: contacts, recipes...

---
+++ public/scripts/recipes.js
@@ -0,0 +1,34 @@
+// main.js
+document.addEventListener("DOMContentLoaded", async function () {
+  console.info("DOM loaded");
+
+  
+
+  // User login status and navigation update
+  const loginContainer = document.getElementById("loginContainer");
+  try {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.name) {
+      console.log("User logged in", user.name);
+      loginContainer.innerHTML = "";
+
+      const userNameDisplay = document.createElement("span");
+      userNameDisplay.textContent = `${user.name} (connected) `;
+      loginContainer.appendChild(userNameDisplay);
+
+      const logoutButton = document.createElement("button");
+      logoutButton.textContent = "Logout";
+      logoutButton.onclick = function () {
+        localStorage.removeItem("user");
+        window.location.href = "login.html";
+      };
+      loginContainer.appendChild(logoutButton);
+    } else {
+      loginContainer.innerHTML = '<a href="login.html">Recettes</a>';
+    }
+  } catch (e) {
+    console.error("Error parsing user data:", e);
+    loginContainer.innerHTML = '<a href="login.html">Recettes</a>';
+  }
+  
+});
